239d66e368fc6a9f1dbfe8a07c4845f5c3ec42d8
FabHaar <fabienhaar@gmail.com>
2023-11-07 16:27:18 +0100

    update create db et sae51.sh

--- create_db.sh
+++ create_db.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+file=$(ls -gotR shared/*.sql | head -n 1 | awk '{print $4}')
 
-#Connection au SGBD pour la création de la base de données à partir du fichie build_db.sql
-mysql -u root -h 127.0.0.1 --port=3306 < build_db.sql
+#Connection au SGBD pour la création de la base de données à partir du fichier sql le plus récent provenant de schema dbml
+mysql -u root -h 127.0.0.1 --port=3306 < shared/$file.sql
--- sae51.sh
+++ sae51.sh
@@ -5,10 +5,15 @@ then
 	echo "erreur il faut lancer le script avec les droits super-utilisateur"
 	exit 1
 fi
-#ETAPE 1 :
-docker volume create V_BDD
+
+#Etape 1 :
+docker build -t img_sae51 -f converter/dockerfile
+./converter/run_converter.sh
 
 #ETAPE 2 :
+docker volume create V_BDD
+
+#ETAPE 3 :
 echo "Création du conteneur du SGBD"
 docker run --name SQL_Server \
 	-p 3306:3306 \
@@ -23,15 +28,15 @@ sleep 40          #attente du socket ready
 echo "vérification des prérequis à la connexion"
 ./bypass_warning.sh
 
-#ETAPE 3 :
+#ETAPE 4 :
 echo "Création de la base de données sur le SGBD..."
 ./create_db.sh
 
-#ETAPE 4 :
+#ETAPE 5 :
 echo "Remplissage de la base de données"
 ./filldb.sh
 
 
-#ETAPE 5 :
+#ETAPE 6 :
 cd select_sql
 ./exemples.sh
