8982cf6d8d643923c26a6c5728eace719fa540c0
FabHaar <fabienhaar@gmail.com>
2023-11-07 15:27:44 +0100

    converter again

---
+++ gen_sql.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for a in *.dbml
+do
+	name=${a.%dbml}
+	echo "processing $name"
+	dbml2sql $a --mysql > $name.sql
+	dbml-renderer $a $name.svg
+done
+
+#dbml-renderer -i example.dbml -o output.svg
