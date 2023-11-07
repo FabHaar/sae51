f3a14cf31600975c513159b6acda341ea7720cfb
FabHaar <fabienhaar@gmail.com>
2023-11-07 15:24:35 +0100

    dockerfile et converter

--- converter/run_converter.sh
+++ converter/run_converter.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-docker run -it \
+mkdir -p shared/
+cp *.dbml shared/
+cp gen_sql.sh shared/
+
+docker run --rm -it \
 	--workdir /srv \
 	--name sae51-ub \
 	--mount type=bind, sources=$(pwd)/shared, target=/srv \
 	img_sae51
+
+apt-get -y install imagemagick
+
+cd shared
+for a in *.svg
+do
+	name=${a%.svg}
+	echo "converting $name"
+	convert $a $name.png
+done
